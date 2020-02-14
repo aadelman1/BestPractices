223282e6fdcab73370a4b16c73a92761c07b1155
aadelman1 <60869329+aadelman1@users.noreply.github.com>
2020-02-14 14:34:59 -0800

    Update 11 - BestPractices.R

--- 11 - BestPractices.R
+++ 11 - BestPractices.R
@@ -1,32 +1,7 @@
 N <- 10
-B <- round(rnorm(1, 3, 1), 0)
+
+for(i in 1:100) {
+ B <- round(rnorm(1, 3, 1), 0)
 D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
+N <- N + B - D}
 print(N)
